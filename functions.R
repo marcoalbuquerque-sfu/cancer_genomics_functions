7b8461dfe99f9a91a23c92977ecb09f8c9c97826
Marco Albuquerque <marco@marco.csclub.uwaterloo.ca>
2016-01-29 12:48:20 -0800

    Update

--- functions.R
+++ functions.R
@@ -1,3 +1,10 @@
+base_pairs <- list(
+    "A" = "T",
+    "C" = "G",
+    "G" = "C",
+    "T" = "A"
+    )
+
 read_biomart_gene_file <- function(biomart_gene_file) {
 
     data <- read.table(
@@ -23,7 +30,7 @@ read_biomart_gene_file <- function(biomart_gene_file) {
 
     }
 
-read_titan_cnv_file <- function(titan_cnv_file) {
+read_titan_cnv_file <- function(titan_cnv_file, only_major=TRUE, all=FALSE, keep_na=FALSE) {
 
     data <- read.table(
         file=titan_cnv_file,
@@ -31,19 +38,64 @@ read_titan_cnv_file <- function(titan_cnv_file) {
         stringsAsFactors=F
     	)
 
-    data <- data[,c(2,3,4,1,7,9,10)]
+    if ( only_major ) {
 
-    colnames(data) <- c(
-        "chr",
-        "start",
-        "end",
-        "sample",
-        "logR",
-        "state",
-        "copy"
-    	)
+        truth <- data[,13] == 1;
+        
+        if ( keep_na ) {
+            truth[is.na(truth)] <- TRUE
+            }
+        
+        else {
+            truth[is.na(truth)] <- FALSE
+            }
+        
+        data <- data[truth, ]
+        
+        }
+
+    if ( !all ) {
+        data <- data[,c(2,3,4,1,7,9,10)]
+
+        colnames(data) <- c(
+            "chr",
+            "start",
+            "end",
+            "sample",
+            "logR",
+            "state",
+            "copy"
+        	)
+        }
 
     return(data)
+
+    }
+
+generate_cohort_cnv_file <- function(list_of_files, method="titan", ...) {
+
+    if ( method == "titan" ) {
+        data <- lapply(list_of_files, read_titan_cnv_file, ...);
+        data <- Reduce(f=rbind, x=data)
+        return(data);
+        }
+
+    }
+
+remap_states <- function(cnv_file) {
+
+    copy <- cnv_file$copy;
+
+    new_state <- rep("", length(copy))
+    new_state[copy == 0] <- "HOMD";
+    new_state[copy == 1] <- "HETD";
+    new_state[copy == 2] <- "NEUT";
+    new_state[copy == 3] <- "GAIN";
+    new_state[copy >= 4] <- "AMP";
+
+    cnv_file$state <- new_state;
+
+    return(cnv_file)
 
     }
 
@@ -97,6 +149,150 @@ calculate_percent_genome_alteration <- function(cnv_frame, genome_frame) {
 
     }
 
+calculate_gene_snv_counts <- function(snv_frame, best=FALSE) {
+
+    data <- snv_frame[,c('gene','patient')];
+    binned_data <- table(unique(data)$gene, exclude="Unknown");
+    order <- order(binned_data, decreasing=TRUE);
+    output <- binned_data[order];
+    if (best) {
+        output <- output[ output >= mean(output) + 4*sd(output) ];
+        }
+
+    output <- data.frame(
+        gene = names(output),
+        counts = as.numeric(output),
+        stringsAsFactors = F
+        )
+
+    rownames(output) <- output$gene;
+
+    return(output);    
+
+    }
+
+calculate_patient_snv_counts <- function(snv_frame) {
+
+    data <- snv_frame[,c('patient')];
+    binned_data <- table(data);
+    order <- order(binned_data, decreasing=TRUE);
+    output <- binned_data[order];
+
+    output <- data.frame(
+        patient = names(output),
+        counts = as.numeric(output),
+        stringsAsFactors = F
+        )
+
+    rownames(output) <- output$patient
+
+    return(output);
+
+    }
+
+calculate_patient_snv_frequency <- function(snv_frame) {
+
+    data <- snv_frame[,c('patient','ref','alt','variant_type')];
+    data <- data[data$variant_type == "SNP",];
+
+    print ("A");
+
+    truth <- data$ref == "A";
+    data[truth,]$ref = "T";
+    data[truth,]$alt = as.character(base_pairs[data[truth,]$alt]);
+
+    truth <- data$ref == "G";
+    data[truth,]$ref = "C";
+    data[truth,]$alt = as.character(base_pairs[data[truth,]$alt]);
+
+    print ("B");
+
+    data <- data.frame(
+        patient = data$patient,
+        snv = paste(data$ref, data$alt, sep=">"),
+        stringsAsFactors=F
+        )
+
+    output <- data.frame(
+        patient = rep(unique(data$patient), each=6),
+        category = rep(c('C>A', 'C>G', 'C>T', 'T>A', 'T>C', 'T>G'), by=6),
+        counts = rep(0, length(unique(data$patient)) * 6),
+        stringsAsFactors = F
+        );
+
+    tmp <- table(data);
+
+    print(tmp);
+
+    print ("C");
+
+    num_vars <- apply(tmp, 1, sum);
+
+    tmp <- tmp / num_vars;
+
+    for ( patient in rownames(tmp) ) {
+        for ( category in colnames(tmp) ) {
+            output[output$patient == patient & output$category == category,]$counts <- tmp[patient, category];
+            }
+        }
+
+    print ("D");
+
+    return(output);
+
+    }
+
+read_vcf2maf_maf <- function(vcf2maf_file) {
+
+    data <- read.table(
+        vcf2maf_file,
+        stringsAsFactors=F,
+        header=T,
+        comment.char="#",
+        sep="\t",
+        fill=T,
+        quote='"'
+        )
+
+    output <- data.frame(
+        gene = data$Hugo_Symbol,
+        patient = data$Tumor_Sample_Barcode,
+        chr = data$Chromosome,
+        start = data$Start_Position,
+        end = data$End_Position,
+        variant_class = data$Variant_Classification,
+        variant_type = data$Variant_Type,
+        ref = data$Reference_Allele,
+        alt = data$Tumor_Seq_Allele2,
+        impact = rep("", nrow(data)),
+        stringsAsFactors = F
+        )
+
+    if (any("IMPACT" == colnames(data))) {
+        output$impact = data$IMPACT;
+        }
+
+    return(output)
+
+    }
+
+read_gene_list <- function(gene_list_file) {
+
+    data <- read.table(
+        gene_list_file,
+        stringsAsFactors=F
+        )
+
+    output <- data.frame(
+        gene = data$V1,
+        stringsAsFactors=F
+        )
+
+    return(output)
+
+    }
+
+
 read_mutsigCV_genes <- function(mutsigCV_file) {
 
     data <- read.table(
@@ -112,6 +308,8 @@ read_mutsigCV_genes <- function(mutsigCV_file) {
         "p.value",
         "q.value"
         )
+
+    rownames(data) <- data$gene;
 
     return(data)
 
@@ -133,6 +331,10 @@ read_oncodriveFM_genes <- function(oncodriveFM_file) {
         "q.value"=data[,2],
         stringsAsFactors=F
     	)
+
+    rownames(data) <- data$gene;
+
+    data <- data[order(data$p.value),]
 
     return(data)
 
@@ -160,7 +362,7 @@ join_gene_list <- function(mutsig=NULL, oncodrive=NULL, other=NULL) {
             data <- oncodrive;
             }
         else {
-            data <- rbind(data, oncodrive);calculate
+            data <- rbind(data, oncodrive);
             }
 	    }
 
@@ -193,7 +395,46 @@ read_titan_params_file <- function(params_file) {
 
 fetch_validity_index <- function(params_frame) {
 
-    truth <- data[,1] == "S_Dbw" & data[,2] == "validity" &  data[,3] == "index" & data[,4] == "(Both):";
+    truth <- params_frame[,1] == "S_Dbw" & params_frame[,2] == "validity" &  params_frame[,3] == "index" & params_frame[,4] == "(Both):";
 
     return(as.numeric(params_frame[truth,5]))
+    }
+
+write_cnv_frame <- function(cnv_frame, output, for_circos=FALSE) {
+
+    if ( for_circos ) {
+        
+        data <- data.frame(
+            sample = cnv_frame$sample,
+            chr = cnv_frame$chr,
+            start = cnv_frame$start,
+            end = cnv_frame$end,
+            n.mark = rep(1, nrow(cnv_frame)),
+            logR = cnv_frame$logR,
+            state = cnv_frame$state,
+            stringsAsFactors = FALSE
+            );
+
+        write.table(
+            x = data,
+            file = output,
+            quote = FALSE,
+            row.names = FALSE,
+            col.names = FALSE,
+            sep ="\t"
+            );
+
+        }
+    else {
+
+        write.table(
+            x = cnv_frame,
+            file = output,
+            quote = FALSE,
+            row.names = FALSE,
+            col.names = TRUE,
+            sep = "\t"
+            )
+        }
+
     }
